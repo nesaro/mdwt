8ae2be19f0534017e7a885994a77991b73578a44
Nestor Arocha <n@nestorarocha.com>
2026-07-14 13:20:19 +0100

    add time functions

---
+++ mdwt/time.py
@@ -0,0 +1,13 @@
+import datetime
+import glob
+from .mdwt import WIKI_HOME
+
+def print_links_to_day_file(day_string, prefix=""):
+    import os.path
+    day_without_hyphens = day_string.replace("-", "")
+    list_of_paths = glob.glob(f'**/{day_without_hyphens}*.md', root_dir=WIKI_HOME, recursive=True)
+    list_of_files = [os.path.basename(x) for x in list_of_paths]
+    list_of_files_no_extension = sorted([os.path.splitext(x)[0] for x in list_of_files])
+    for doc in list_of_files_no_extension:
+        cog.outl(f"{prefix}* [[{doc}]]")
+    
